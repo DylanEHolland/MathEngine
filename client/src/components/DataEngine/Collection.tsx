a29669f2451463ae2de44f7d2ab3ae5279b832ec
DylanEHolland <salinson1138@gmail.com>
2022-05-21 22:50:20 -0700

    Continuing work on the data subsystem

--- client/src/components/DataEngine/Collection.tsx
+++ client/src/components/DataEngine/Collection.tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import DataObject from "./DataObject";
 
-export default class Collection extends React.Component {
+const defaultProps = {
+    objects: null,
+    orderedObjects: null
+}
+
+export interface CollectionState {
+    objects: any;
+    orderedObjects: any;
+};
+
+export interface CollectionProps {
+    dedup?: boolean;
+    atomic?: boolean;
+    sequential?: boolean;
+    finite?: boolean;
+};
+
+export default class Collection extends React.Component<CollectionProps, CollectionState> {
+    state = {
+        objects: null,
+        orderedObjects: null
+    };
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps: any, prevState: any) {
+        parseIncomingProps(this.setState, defaultProps, this.props);
+    }
+
     render = () => {
+        // let {objects} = this.state;
+        // console.log("test: help", objects)
+
         return (
-            <>
-                <DataObject />
-            </>
+            <div className="collection">
+                <DataObject  />
+            </div>
         );
     }
 }
+
+const parseIncomingProps = (setState: any, defaultProps: any, props: any) => {
+
+} 
--- client/src/components/DataEngine/Data.tsx
+++ client/src/components/DataEngine/Data.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react"
 import Collection from "./Collection"
 
 export const Data = () => {
+    const [dataName, setDataName] = useState<string|null>(null);
+
     return (
-        <>
+        <div className="data">
+            <em>{dataName ? dataName : "Nameless Dataset"}</em>
+            
             <Collection />
-        </>
+        </div>
     )
 }
